29bc7ee3e32dae28c830b16a3777522796a0f39f
masafumi kato <xslash1105@gmail.com>
2021-03-06 20:07:55 +0900

    [move] frontendにNuxtコードを移動

---
+++ frontend/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    Hello world
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'App',
+  components: {
+
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+})
+</script>
