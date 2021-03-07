3ae826d4eb75d6d38757a3fb0193c1493089be95
masafumi kato <xslash1105@gmail.com>
2021-03-07 18:56:53 +0900

    [update] 取得データの表示

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
     Hello world
+    {{person ? person.name : ''}}
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import firstPersonGql from '~/apollo/queries/firstPerson.graphql';
-import ggl from 'graphql-tag'
 
 export default Vue.extend({
   name: 'App',
@@ -16,6 +16,7 @@ export default Vue.extend({
   },
   data () {
     return {
+      person: {},
     }
   },
   async created() {
@@ -26,15 +27,18 @@ export default Vue.extend({
   },
   methods: {
     async start() {
-      const result = await this.$apollo.query({
+      this.person = await this.$apollo.query({
         query: firstPersonGql,
         variables: {
           personID: 1,
         }
+      }).then(({data}) => {
+        return data.person ?? {}
       }).catch((err) => {
-        console.error(err)
+        console.log(err)
+        return {}
       })
-      console.log(result);
+      console.log(this.person);
     }
   }
 })
